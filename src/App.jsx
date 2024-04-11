168f0f839ec5767e128ae6a273ef83c015887db6
Mohammad Mohammad <m.mohammad25@outlook.com>
2024-04-11 22:25:00 +0200

    after styling the background and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
-    <div className=''>App</div>
-  )
-}
+    <div className="w-[90vw] h-[90vh] bg-[rgba(17,25,40,0.75)] rounded-xl backdrop-blur-[19px] backdrop-saturate-[180%] border border-white/[.125] border-solid ">
+      App
+    </div>
+  );
+};
 
-export default App
+export default App;
