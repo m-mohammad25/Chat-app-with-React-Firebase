9911ec6137d3d37ae506ada1a2d1935665908568
Mohammad Mohammad <m.mohammad25@outlook.com>
2024-04-15 21:10:01 +0200

    after building bottom and top sections of chat component

--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -1,7 +1,83 @@
+import EmojiPicker from "emoji-picker-react";
+import { useState } from "react";
 const Chat = () => {
+  const [openEmoji, setOpenEmoji] = useState(false);
+  const [message, setMessage] = useState("");
+  function handleEmoji(e) {
+    setMessage((prev) => prev + e.emoji);
+    setOpenEmoji(false);
+  }
   return (
-    <div className=" flex-[2] border-x border-solid border-x-[#dddddd35]">
-      Chat
+    <div className=" flex-[2] border-x border-solid border-x-[#dddddd35] h-full">
+      <div className="top flex items-center justify-between p-5 border-b border-solid border-b-[#dddddd35]">
+        <div className="user flex items-center gap-5">
+          <img
+            className="w-[60px] h-[60px] object-cover rounded-full"
+            src="../../../../public/avatar.png"
+            alt=""
+          />
+          <div className="texts flex flex-col gap-[5px]">
+            <span className="text-lg font-bold">John Doe</span>
+            <p className="text-sm text-[#a5a5a5]">Lorem ipsum dolor sit amet</p>
+          </div>
+        </div>
+        <div className="icons flex gap-5">
+          <img
+            className="w-5 h-5 cursor-pointer"
+            src="../../../phone.png"
+            alt=""
+          />
+          <img
+            className="w-5 h-5 cursor-pointer"
+            src="../../../video.png"
+            alt=""
+          />
+          <img
+            className="w-5 h-5 cursor-pointer"
+            src="../../../edit.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="bottom p-5 gap-5 flex items-center content-between border-solid border-t border-t-[#dddddd35] ">
+        <div className="icons flex gap-5">
+          <img
+            className="w-5 h-5 cursor-pointer"
+            src="../../../img.png"
+            alt=""
+          />
+          <img
+            className="w-5 h-5 cursor-pointer"
+            src="../../../camera.png"
+            alt=""
+          />
+          <img
+            className="w-5 h-5 cursor-pointer"
+            src="../../../mic.png"
+            alt=""
+          />
+        </div>
+        <input
+          type="text"
+          placeholder="send a message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          className="flex-1 border-none outline-none text-[16px] text-white p-[10px] rounded-[10px] bg-[rgba(17,25,40,.5)] "
+        />
+        <div className="emoji">
+          <img
+            className="w-5 h-5 cursor-pointer"
+            src="../../../emoji.png"
+            alt=""
+            onClick={() => setOpenEmoji((prev) => !prev)}
+          />
+          <EmojiPicker open={openEmoji} onEmojiClick={handleEmoji} />
+        </div>
+        <button className="bg-[#5183fe] text-white border-none py-[10px] px-5 cursor-pointer rounded-md">
+          Send
+        </button>
+      </div>
     </div>
   );
 };
