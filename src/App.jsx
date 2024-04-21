e72ac7afe3b7238d0fbde5fafecff00c8956437b
Mohammad Mohammad <m.mohammad25@outlook.com>
2024-04-21 20:18:34 +0200

    after making login and registeration

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Login from "./Components/Login/Login";
 import Notification from "./Components/Notification/Notification";
 
 const App = () => {
-  const user = true;
+  const user = false;
   return (
     <div className="w-[80vw] h-[90vh] bg-[rgba(17,25,40,0.75)] rounded-xl backdrop-blur-[19px] backdrop-saturate-[180%] border border-white/[.125] border-solid flex">
       {user ? (
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { toast } from "react-toastify";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../../lib/firebase";
+import { doc, setDoc } from "firebase/firestore";
 const Login = () => {
   const [avatar, setAvatar] = useState({
     file: null,
@@ -12,6 +15,33 @@ const Login = () => {
       file: e.target.files[0],
       url: URL.createObjectURL(e.target.files[0]),
     });
+  };
+
+  const handleRegister = async function (e) {
+    e.preventDefault();
+    const formDate = new FormData(e.target);
+    const { username, password, email } = Object.fromEntries(formDate);
+
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(res.user);
+
+      await setDoc(doc(db, "users", res.user.uid), {
+        username,
+        email,
+        id: res.user.uid,
+        blocked: [],
+      });
+
+      await setDoc(doc(db, "userChats", res.user.uid), {
+        chats: [],
+      });
+
+      toast.success("Account created! You can login now");
+    } catch (e) {
+      console.log(e.message);
+      toast.error(e.message);
+    }
   };
 
   const handleLogin = function (e) {
@@ -46,7 +76,10 @@ const Login = () => {
       <div className="separator h-[80%] w-[2px] bg-[#dddddd35] rounded-sm"></div>
       <div className="flex flex-col items-center flex-1 gap-5 item">
         <h2>Create an Account</h2>
-        <form className="flex flex-col items-center justify-center flex-1 gap-5">
+        <form
+          onSubmit={handleRegister}
+          className="flex flex-col items-center justify-center flex-1 gap-5"
+        >
           <label
             htmlFor="file"
             className="flex items-center justify-between w-full underline cursor-pointer"
@@ -67,7 +100,7 @@ const Login = () => {
           <input
             type="text"
             placeholder="Username"
-            name="email"
+            name="username"
             className="p-5 border-none outline-none bg-[rgba(17,25,40,0.6)] text-white rounded-md"
           />
           <input
