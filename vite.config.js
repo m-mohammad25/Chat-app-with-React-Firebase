749632b0530b81c431bf1203902b59d55930e783
Mohammad Mohammad <m.mohammad25@outlook.com>
2024-05-28 23:36:36 +0300

    added base to vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  name: "/Chat-app-with-React-Firebase/",
+  base: "/Chat-app-with-React-Firebase/",
   plugins: [react()],
 });
